342c8ddf4ec0ceb1c57b95aed2f36d42aaea4f5b
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-07-31 17:20:37 +0200

    First Amazon Clone commit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyANiLH4BqE8icwhqWgEM7LmydSwXcw4DRs",
+    authDomain: "clone-two-6e6b9.firebaseapp.com",
+    projectId: "clone-two-6e6b9",
+    storageBucket: "clone-two-6e6b9.appspot.com",
+    messagingSenderId: "847398608942",
+    appId: "1:847398608942:web:39b25cd4caa21755463fbf",
+    measurementId: "G-56CWSH6S5G"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
